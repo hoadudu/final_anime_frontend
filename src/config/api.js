f7cb51395eccf6ab5152730af874441e9c587c88
hoadudu <65750571+hoadudu@users.noreply.github.com>
2025-10-18 11:47:27 +0700

    ok

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = 'http://localhost:8000/api' // Using mock API server
-export const API_BASE_URL = 'https://dev.anime47.best/api' // Using mock API server
+export const API_BASE_URL = 'https://anime47.love/api' // Using mock API server
 export const API_MOCK_URL = 'http://localhost:3000' // Mock API server
-export const APP_NAME = 'AnimeVui'
+export const APP_NAME = 'ANIME47'
